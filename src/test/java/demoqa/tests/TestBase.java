0727296cd460d9b3449605c9787d8fbca9627a66
Леха <aorlov@sberhealth.ru>
2022-10-24 12:25:39 +0300

    1

--- src/test/java/demoqa/tests/TestBase.java
+++ src/test/java/demoqa/tests/TestBase.java
@@ -7,6 +7,7 @@ public class TestBase {
 
     @BeforeAll
     static void setUp() {
+        Configuration.holdBrowserOpen = true;
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
 
