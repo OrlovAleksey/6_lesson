88790a0078538b1ce9b4a660e2153feca640aaa1
Леха <aorlov@sberhealth.ru>
2022-10-24 23:03:30 +0300

    1

--- src/test/java/demoqa/tests/TestData.java
+++ src/test/java/demoqa/tests/TestData.java
@@ -1,23 +1,43 @@
 package demoqa.tests;
 
 import com.github.javafaker.Faker;
-
-import java.time.Month;
-import java.time.format.TextStyle;
 import java.util.Locale;
-import java.util.OptionalInt;
 import java.util.Random;
 
 public class TestData {
-
-
     static Random random = new Random();
+    private static final Faker faker = new Faker(new Locale("en-US"));
 
+    public static String getRandomMonth() {
+        String[] month = {"January", "February", "March", "April",
+                "May", "June", "July", "August",
+                "September", "October", "November", "December"};
+        int randomIndex = random.nextInt(month.length);
+        return month[randomIndex];
+    }
     public static String getRandomGender() {
         String[] genders = {"Male", "Female", "Other"};
         int randomIndex = random.nextInt(genders.length);
         return genders[randomIndex];
     }
-    static String UserGender = getRandomGender();
+    public static String getRandomHobbies() {
+        String[] hobbies = {"Music", "Reading", "Music"};
+        int randomIndex = random.nextInt(hobbies.length);
+        return hobbies[randomIndex];
+    }
+    static String UserGender = getRandomGender(),
+                UserHobbies = getRandomHobbies(),
+                firstName = faker.name().firstName(),
+                lastName = faker.name().lastName(),
+                streetAddress = faker.address().streetAddress(),
+                email = faker.internet().emailAddress(),
+                day = String.valueOf(faker.number().numberBetween(10, 27)),
+                month =getRandomMonth(),
+                year = String.valueOf(faker.number().numberBetween(1900, 2001)),
+                phone = faker.phoneNumber().subscriberNumber(10),
+                subjects = "Hindi",
+                state = "NCR",
+                city = "Noida",
+                picture = "CKtO-Q6I1ks.jpg";
 
 }
--- src/test/java/demoqa/tests/TestRegistrationForm.java
+++ src/test/java/demoqa/tests/TestRegistrationForm.java
@@ -7,13 +7,7 @@ import static demoqa.tests.TestData.*;
 
 public class TestRegistrationForm extends TestBase {
 
-     String firstName = "Alex";
-    String lastName = "Orlov";
-    String email = "aorlov@site.com";
-    String phone = "9777742959";
-    String day = "12";
-    String month = "October";
-    String year = "1996";
+
 
     RegistrationPages registrationPages = new RegistrationPages();
     @Test
@@ -27,23 +21,23 @@ public class TestRegistrationForm extends TestBase {
                 .setGender(UserGender)
                 .setPhone(phone)
                 .setBirthDate(day,month,year)
-                .setSubjects("Hindi")
-                .setHobbies("Music")
+                .setSubjects(subjects)
+                .setHobbies(UserHobbies)
                 .uploadFile()
-                .setAddress("Moskva, Krasnopresnenskaya nab., 12-17")
-                .setStateAndCity("NCR","Noida")
+                .setAddress(streetAddress)
+                .setStateAndCity(state,city)
                 .submitForm();
 
         registrationPages.checkResultsTableVisible()
-                .checkResultTable("Student Name", firstName +" "+ lastName)
+                .checkResultTable("Student Name", firstName + " " + lastName)
                 .checkResultTable("Student Email", email)
-                .checkResultTable("Gender", "Male")
+                .checkResultTable("Gender", UserGender)
                 .checkResultTable("Mobile", phone)
                 .checkResultTable("Date of Birth", day + " "+ month +","+year)
-                .checkResultTable("Subjects", "Hindi")
-                .checkResultTable("Hobbies", "Music")
-                .checkResultTable("Picture", "CKtO-Q6I1ks.jpg")
-                .checkResultTable("Address", "Moskva, Krasnopresnenskaya nab., 12-17")
-                .checkResultTable("State and City", "NCR Noida");
+                .checkResultTable("Subjects", subjects)
+                .checkResultTable("Hobbies", UserHobbies)
+                .checkResultTable("Picture", picture)
+                .checkResultTable("Address", streetAddress)
+                .checkResultTable("State and City", state + " " + city);
     }
 }
